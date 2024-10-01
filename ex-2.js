88a000da67dd7c41e834fb59c606df94bd46612e
Mocca <p0n-@hotmail.com>
2024-10-01 13:45:30 +0700

    Mocca assignment

--- ex-2.js
+++ ex-2.js
@@ -8,3 +8,29 @@ const product = {
 };
 let socialMediaShare = "facebookShare";
 // Start code here
+console.log(product)
+
+product.ratings = 4.5;
+console.log(product.ratings);
+
+product.photo = "https://i01.appmifile.com/webfile/globalimg/products/pc/mi-air-purifier-3H/replace_03.jpg";
+console.log(product.photo);
+
+product["code"] = undefined; //ไม่ต้องมีกะได้ถ้ามะได้ดึงค่าจาก Variable เเละกำหนด value ให้เลย
+product.code = "IT0101010";
+console.log(product);
+
+product.price = 6000;
+console.log(product.price);
+
+delete product.ratings;
+console.log(Object.keys(product));
+
+
+console.log(socialMediaShare)
+product[socialMediaShare] = undefined;
+console.log(Object.keys(product));
+
+product.socialMediaShare = 45.5;
+console.log(product.socialMediaShare);
+
